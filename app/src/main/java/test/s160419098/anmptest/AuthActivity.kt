3130613088a6884915156366d35f5173add8723c
Wafi Hartono <=>
2023-08-28 14:45:28 +0700

    Add auth feature

---
+++ app/src/main/java/test/s160419098/anmptest/AuthActivity.kt
@@ -0,0 +1,11 @@
+package test.s160419098.anmptest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
